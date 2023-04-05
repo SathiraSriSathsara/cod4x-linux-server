7b27c117461fd200a5f8888e515fad144918ba20
Sathira Sri Sathara <sathira.professional@gmail.com>
2023-04-06 03:56:48 +0530

    Add files via upload

---
+++ call_of_duty_4_server_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Script by Sathira Sri Sathsara (SaM)"
+
+
+# Update the system and install dependencies
+dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+sudo apt-get install unzip -y
+
+# Get gdown and python pip
+apt install python-pip
+pip install gdown
+
+# Download the server files
+cd /home
+mkdir servers
+cd /home/servers
+gdown 1YhrP8OV0l-LKJKHC6BoRg7pQ1Bki4zYd
+unzip cod4x-linux-server.zip
+rm cod4x-linux-server.zip
+cd /home/servers/cod4x-linux-server
+
+# Start the server
+ chmod +x start.sh
+./start.sh
+
+# Detach from the screen session
+Ctrl-A, Ctrl-D
+
+
+echo "Server Started Enjoy <3 [SaM]"
