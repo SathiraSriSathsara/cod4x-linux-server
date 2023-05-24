518cb6d45c522fa3dd40751441454c726b844e77
Sathira Sri Sathara <sathira.professional@gmail.com>
2023-05-25 00:49:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 echo "Script by Sathira Sri Sathsara (SaM)"
 
-
 # Update the system and install dependencies
 sudo dpkg --add-architecture i386
 sudo apt-get update && sudo apt-get upgrade -y
@@ -17,17 +16,13 @@ sudo pip install gdown
 cd /home
 sudo mkdir servers
 cd /home/servers
-sudo gdown 1YhrP8OV0l-LKJKHC6BoRg7pQ1Bki4zYd
+sudo gdown --id 1YhrP8OV0l-LKJKHC6BoRg7pQ1Bki4zYd
 sudo unzip cod4x-linux-server.zip
 sudo rm cod4x-linux-server.zip
 cd /home/servers/cod4x-linux-server
 
 # Start the server
 sudo chmod 777 *
-sudo ./start.sh
-
-# Detach from the screen session
-Ctrl-A, Ctrl-D
-
+sudo screen -S cod4x-server ./start.sh
 
 echo "Server Started Enjoy <3 [SaM]"
